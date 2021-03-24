972bb70b9e54597f55d743683845de11723b6fb5
songrenwei <songrenwei@ehousechina.com>
2021-03-24 14:23:48 +0800

    logback

--- api/src/main/java/com/srw/ApiApplication.java
+++ api/src/main/java/com/srw/ApiApplication.java
@@ -1,21 +1,15 @@
 package com.srw;
 
 import com.dtflys.forest.annotation.ForestScan;
-import com.srw.business.UserService;
-import com.srw.controller.TestController;
-import com.srw.controller.UserController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @ForestScan(basePackages = "com.srw.client")
 @SpringBootApplication
 public class ApiApplication {
 
     public static void main(String[] args) {
-        ConfigurableApplicationContext applicationContext = SpringApplication.run(ApiApplication.class, args);
-        TestController testController = applicationContext.getBean(TestController.class);
-        System.out.println(testController.getClass());
+        SpringApplication.run(ApiApplication.class, args);
     }
 
 }
