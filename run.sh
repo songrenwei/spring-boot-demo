c86b671a3cf0fd97406f5ad8314b93d67d586dba
songrenwei <songrenwei@ehousechina.com>
2021-03-26 13:39:54 +0800

    部署04

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # 定义应用组名
-group_name='boot-stage'
+# group_name='mall'
 # 定义应用名称
 app_name='api'
 # 定义应用版本
@@ -12,16 +12,17 @@ docker stop ${app_name}
 echo '----stop container----'
 docker rm ${app_name}
 echo '----rm container----'
-docker rmi ${group_name}/${app_name}:${app_version}
+docker rmi ${app_name}:${app_version}
 echo '----rm image----'
 # 打包编译docker镜像
-docker build -t ${group_name}/${app_name}:${app_version} .
+docker build -t ${app_name}:${app_version} .
 echo '----build image----'
 docker run -p 8081:8081 --name ${app_name} \
 --link mysql:db \
+--link redis:redis \
 -e 'spring.profiles.active'=${profile_active} \
 -e TZ="Asia/Shanghai" \
 -v /etc/localtime:/etc/localtime \
 -v /mydata/app/${app_name}/logs:/var/logs \
--d ${group_name}/${app_name}:${app_version}
+-d ${app_name}:${app_version}
 echo '----start container----'
