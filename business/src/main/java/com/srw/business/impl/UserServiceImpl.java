ea70d255f87342f9156ab52218209d76a515afc5
songrenwei <songrenwei@ehousechina.com>
2021-03-26 15:17:38 +0800

    部署

--- business/src/main/java/com/srw/business/impl/UserServiceImpl.java
+++ business/src/main/java/com/srw/business/impl/UserServiceImpl.java
@@ -38,7 +38,7 @@ public class UserServiceImpl implements UserService {
     @Log
     @Override
     public List<UserDto> findList() {
-        log.info("是否部署成功了");
+        log.info("你有访问了。。。");
         List<User> list = userMapper.findList();
 
         return Optional.ofNullable(list).orElse(new ArrayList<>()).stream().map(user -> new UserDto(user.getName(), user.getPassword())).collect(Collectors.toList());
